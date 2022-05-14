ee0237aa7e852fa55bef5b019f65f190b0f223c5
a.vinogradov <a.vinogradov@bpcbt.com>
2022-05-14 15:48:27 +0300

    commit

--- frontend/src/components/layouts/GeneralLayout/Navigation/Navigation.tsx
+++ frontend/src/components/layouts/GeneralLayout/Navigation/Navigation.tsx
@@ -8,16 +8,59 @@ interface Props {}
 const Navigation = ({}: Props): JSX.Element => {
   const nodes: TreeNodeInfo[] = [
     {
-      id: 0,
+      id: 'control',
       hasCaret: true,
-      icon: 'folder-close',
-      label: 'Folder 0',
+      icon: 'eye-open',
+      label: 'Контроль',
       isExpanded: true,
       childNodes: [
         {
-          id: 2,
-          icon: 'document',
-          label: 'Item 0',
+          id: 'monitoring',
+          icon: 'desktop',
+          label: 'Мониторинг',
+        },
+        {
+          id: 'registry',
+          icon: 'database',
+          label: 'Реестр',
+        },
+      ],
+    },
+    {
+      id: 'moderate',
+      hasCaret: true,
+      icon: 'edit',
+      label: 'Управление',
+      isExpanded: true,
+      childNodes: [
+        {
+          id: 'apps',
+          icon: 'graph',
+          label: 'Приложения',
+        },
+        {
+          id: 'orgs',
+          icon: 'heat-grid',
+          label: 'Организации',
+        },
+      ],
+    },
+    {
+      id: 'administrate',
+      hasCaret: true,
+      icon: 'build',
+      label: 'Администрирование',
+      isExpanded: true,
+      childNodes: [
+        {
+          id: 'users',
+          icon: 'people',
+          label: 'Пользователи',
+        },
+        {
+          id: 'history',
+          icon: 'history',
+          label: 'История действий',
         },
       ],
     },
