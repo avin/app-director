4e200a14d2938725057e4c414a9e55077e5d5a4f
a.vinogradov <a.vinogradov@bpcbt.com>
2022-05-15 15:00:44 +0300

    commit

--- frontend/src/components/layouts/GeneralLayout/Header/UserButton/UserButton.tsx
+++ frontend/src/components/layouts/GeneralLayout/Header/UserButton/UserButton.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import styles from './UserButton.module.scss';
-import { Button } from '@blueprintjs/core';
+import React, { useCallback } from 'react';
+import { Button, Menu, MenuItem, Position } from '@blueprintjs/core';
+import { Popover2 } from '@blueprintjs/popover2';
 
 interface Props {}
 
 const UserButton = ({}: Props): JSX.Element => {
+  const handleClickLogout = useCallback(() => {
+    // TODO
+  }, []);
+
   return (
-    <Button icon="user" minimal>
-      foo@asd.com
-    </Button>
+    <Popover2
+      minimal
+      modifiers={{ offset: { enabled: true, options: { offset: [0, 10] } } }}
+      content={
+        <Menu>
+          <MenuItem icon="log-out" onClick={handleClickLogout} text="Выйти" />
+        </Menu>
+      }
+      position={Position.BOTTOM_LEFT}
+    >
+      <Button icon="user" minimal>
+        foo@asd.com
+      </Button>
+    </Popover2>
   );
 };
 
